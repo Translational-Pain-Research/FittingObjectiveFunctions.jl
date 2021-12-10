50bcac405dfea8833547a85f7212c3005aa3179d
Dominik Tschimmel <dominik.tschimmel.contact@gmail.com>
2021-12-10 16:19:44 +0100

    Initialize Package

---
+++ src/FittingObjectiveFunctions.jl
@@ -0,0 +1,5 @@
+module FittingObjectiveFunctions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FittingObjectiveFunctions
+using Test
+
+@testset "FittingObjectiveFunctions.jl" begin
+    # Write your tests here.
+end
